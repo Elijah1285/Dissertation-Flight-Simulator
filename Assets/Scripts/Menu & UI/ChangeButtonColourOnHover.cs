32e7ba25387760954a77830f57a16b7fbc009cd1
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2025-05-04 17:53:15 +0100

    work on selection menu

---
+++ Assets/Scripts/Menu & UI/ChangeButtonColourOnHover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangeButtonColourOnHover : MonoBehaviour
+{
+    [SerializeField] Graphic graphic;
+    [SerializeField] AudioClip blip_sound;
+    [SerializeField] AudioSource audio_source;
+
+    public void hoveredOverButton()
+    {
+        graphic.color = Color.yellow;
+        audio_source.PlayOneShot(blip_sound);
+    }
+
+    public void unhoveredOverButton()
+    {
+        graphic.color = Color.white;
+    }
+}
--- Assets/Scripts/Menu & UI/SelectionScreenController.cs
+++ Assets/Scripts/Menu & UI/SelectionScreenController.cs
@@ -5,34 +5,146 @@ using UnityEngine.SceneManagement;
 
 public class SelectionScreenController : MonoBehaviour
 {
-    public void selectCessna172SP()
+    public enum AircraftType
     {
-        PlayerPrefs.SetString("chosen_aircraft", "cessna_172sp");
+        CESSNA_172SP = 1,
+        PIPER_SUPER_CUB = 2,
+        DHC_DASH_8_Q400 = 3,
+        BOEING_737_800 = 4,
+        BOEING_747_400F = 5
+    }
+
+    public enum SelectionScreen
+    {
+        MAIN_SCREEN = 1,
+        CESSNA_172_SP_INFO = 2,
+        PIPER_SUPER_CUB_INFO = 3,
+        DHC_DASH_8_Q400_INFO = 4,
+        BOEING_737_800_INFO = 5,
+        BOEING_747_400F_INFO = 6
+    }
+
+    [SerializeField] GameObject main_screen;
+    [SerializeField] GameObject cessna_172sp_info_screen;
+    [SerializeField] GameObject piper_pa18_super_cub_info_screen;
+    [SerializeField] GameObject dhc_dash_8_q400_info_screen;
+    [SerializeField] GameObject boeing_737_800_info_screen;
+    [SerializeField] GameObject boeing_747_400f_info_screen;
+
+    public void selectAircraft(int aircraft_index)
+    {
+        AircraftType aircraft = (AircraftType) aircraft_index;
+
+        switch (aircraft)
+        {
+            case AircraftType.CESSNA_172SP:
+                {
+                    PlayerPrefs.SetString("chosen_aircraft", "cessna_172sp");
+
+                    break;
+                }
+            case AircraftType.PIPER_SUPER_CUB:
+                {
+                    PlayerPrefs.SetString("chosen_aircraft", "piper_pa18_super_cub");
+
+                    break;
+                }
+            case AircraftType.DHC_DASH_8_Q400:
+                {
+                    PlayerPrefs.SetString("chosen_aircraft", "dehavilland_canada_dash8_q400");
+
+                    break;
+                }
+            case AircraftType.BOEING_737_800:
+                {
+                    PlayerPrefs.SetString("chosen_aircraft", "boeing_737_800");
+
+                    break;
+                }
+            case AircraftType.BOEING_747_400F:
+                {
+                    PlayerPrefs.SetString("chosen_aircraft", "boeing_747_400f");
+
+                    break;
+                }
+        }
+
         loadGame();
     }
 
-    public void selectPiperPA18SuperCub()
+    public void switchScreen(int screen_index)
     {
-        PlayerPrefs.SetString("chosen_aircraft", "piper_pa18_super_cub");
-        loadGame();
-    }
+        SelectionScreen screen = (SelectionScreen) screen_index;
 
-    public void selectDeHavillandCanadaDash8Q400()
-    {
-        PlayerPrefs.SetString("chosen_aircraft", "dehavilland_canada_dash8_q400");
-        loadGame();
-    }
+        switch (screen)
+        {
+            case SelectionScreen.MAIN_SCREEN:
+                {
+                    main_screen.SetActive(true);
+                    cessna_172sp_info_screen.SetActive(false);
+                    piper_pa18_super_cub_info_screen.SetActive(false);
+                    dhc_dash_8_q400_info_screen.SetActive(false);
+                    boeing_737_800_info_screen.SetActive(false);
+                    boeing_747_400f_info_screen.SetActive(false);
 
-    public void selectBoeing737_800()
-    {
-        PlayerPrefs.SetString("chosen_aircraft", "boeing_737_800");
-        loadGame();
-    }
+                    break;
+                }
+            case SelectionScreen.CESSNA_172_SP_INFO:
+                {
+                    main_screen.SetActive(false);
+                    cessna_172sp_info_screen.SetActive(true);
+                    piper_pa18_super_cub_info_screen.SetActive(false);
+                    dhc_dash_8_q400_info_screen.SetActive(false);
+                    boeing_737_800_info_screen.SetActive(false);
+                    boeing_747_400f_info_screen.SetActive(false);
 
-    public void selectBoeing747_400F()
-    {
-        PlayerPrefs.SetString("chosen_aircraft", "boeing_747_400f");
-        loadGame();
+                    break;
+                }
+            case SelectionScreen.PIPER_SUPER_CUB_INFO:
+                {
+                    main_screen.SetActive(false);
+                    cessna_172sp_info_screen.SetActive(false);
+                    piper_pa18_super_cub_info_screen.SetActive(true);
+                    dhc_dash_8_q400_info_screen.SetActive(false);
+                    boeing_737_800_info_screen.SetActive(false);
+                    boeing_747_400f_info_screen.SetActive(false);
+
+                    break;
+                }
+            case SelectionScreen.DHC_DASH_8_Q400_INFO:
+                {
+                    main_screen.SetActive(false);
+                    cessna_172sp_info_screen.SetActive(false);
+                    piper_pa18_super_cub_info_screen.SetActive(false);
+                    dhc_dash_8_q400_info_screen.SetActive(true);
+                    boeing_737_800_info_screen.SetActive(false);
+                    boeing_747_400f_info_screen.SetActive(false);
+
+                    break;
+                }
+            case SelectionScreen.BOEING_737_800_INFO:
+                {
+                    main_screen.SetActive(false);
+                    cessna_172sp_info_screen.SetActive(false);
+                    piper_pa18_super_cub_info_screen.SetActive(false);
+                    dhc_dash_8_q400_info_screen.SetActive(false);
+                    boeing_737_800_info_screen.SetActive(true);
+                    boeing_747_400f_info_screen.SetActive(false);
+
+                    break;
+                }
+            case SelectionScreen.BOEING_747_400F_INFO:
+                {
+                    main_screen.SetActive(false);
+                    cessna_172sp_info_screen.SetActive(false);
+                    piper_pa18_super_cub_info_screen.SetActive(false);
+                    dhc_dash_8_q400_info_screen.SetActive(false);
+                    boeing_737_800_info_screen.SetActive(false);
+                    boeing_747_400f_info_screen.SetActive(true);
+
+                    break;
+                }
+        }
     }
 
     void loadGame()
